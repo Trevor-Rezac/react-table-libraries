d3860026593aa7d3dc290a823032e2207fc3d379
TrevorRezac <trev.rezac@gmail.com>
2022-02-21 18:10:54 -0800

    created data.js and set up a basic table with MaterialTable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import MaterialTable from 'material-table';
+import peopleData from './data';
+import columns from './columns';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MaterialTable data={peopleData}
+        columns={columns}
+      />
     </div>
   );
 }
